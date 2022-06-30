ce189c7d4ef0c7a3cbb951cca9ef9c2207b38db6
Pacemaker21 <hardeyke@gmail.com>
2022-06-30 08:56:59 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
+	char Mystr[] = "Programming is like building a multilingual puzzle\n"
+		puts(Mystr);
 		return (0);
 }
