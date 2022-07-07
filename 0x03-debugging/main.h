2a2b20ffef373141ab411974bca7cddd1e31c7dc
Pacemaker21 <hardeyke@gmail.com>
2022-07-07 15:13:21 -0700

    The shortest distance between two points is a straight line

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,5 +9,6 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 void more_numbers(void);
+void print_line(int n);
 
 #endif
